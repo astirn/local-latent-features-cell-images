3c29924448b583d34978330895317543fa0f59c7
Andrew Stirn <andrew.stirn@gmail.com>
2018-05-08 11:31:00 -0400

    updated comments

--- run_mode.py
+++ run_mode.py
@@ -1,5 +1,5 @@
 def test_mode():
-    # true-use to test code on a small swath of data. still requires 16 GB of RAM
+    # true-use to test code on a small swath of data for only a few epochs. still requires 16 GB of RAM.
     return True
 
     # false-use to run code on full data set. requires a significant amount of RAM (100 GB per 400 full-size TIFF's)
