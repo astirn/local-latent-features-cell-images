08588afded14ded77fddd96bd699393e7587f0f8
Andrew Stirn <andrew.stirn@gmail.com>
2018-05-08 13:00:43 -0400

    not in test mode

--- test_mode.py
+++ test_mode.py
@@ -1,6 +1,6 @@
 def test_mode():
     # true-use to test code on a small swath of data for only a few epochs. still requires 16 GB of RAM.
-    return True
+    # return True
 
     # false-use to run code on full data set. requires a significant amount of RAM (100 GB per 400 full-size TIFF's)
-    # return False
+    return False
